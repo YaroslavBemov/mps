cfb43012d54c7bb589b014ff105c1fe3aecd1772
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2022-04-04 13:49:41 +0300

    [WIP]frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import Login from './features/login/Login'
+import OutlinedCard from './components/card/OutlinedCard'
+import MenuAppBar from './components/header/MenuAppBar';
 
 function App() {
+  const [auth, setAuth] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuAppBar />
+      {auth ? (
+        <OutlinedCard />
+      ) : (
+        <Login />
+      )}
+      <Button onClick={()=>setAuth(!auth)}>auth</Button>
     </div>
   );
 }
